070af7397656798c61e43747af7c32f5c78d16a8
PREMAWAISNAWA <premawaisnawa@gmail.com>
2018-01-07 23:47:54 +0800

    CRUD ALternatif
    Create
    Edit
    Update

---
+++ app/src/main/java/com/gita/spk_saw/DataAdapter.java
@@ -0,0 +1,21 @@
+package com.gita.spk_saw;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListAdapter;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class DataAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_adapter);
+
+
+    }
+}
